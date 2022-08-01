846336d374d723286f8d62aeb842d74deaaeb6e6
sachinmhaskar <sachin.mhaskar7@gmail.com>
2022-08-01 17:21:52 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
-}
+const App = () => {
+  return <Expenses />;
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++
@@ -1,5 +0,0 @@
-function ExpenseItem() {
-  return <div>Expense Item!</div>;
-}
-
-export default ExpenseItem;
